544ea3ed1077ff902e04c677fb3104cb44e31c65
Pavel Khokhlov <gg.pavel.kh@gmail.com>
2026-07-01 14:51:46 +0300

    update docker compose

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dahab-frontend/',
-})
+  base: "/",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Автоматический импорт глобальных файлов
+        additionalData: `
+          @import "./src/styles/variables.scss";
+          @import "./src/styles/mixins.scss";
+        `,
+      },
+    },
+    modules: {
+      // Настройка CSS Modules
+      localsConvention: "camelCase",
+      generateScopedName: "[name]__[local]--[hash:base64:5]",
+    },
+  },
+  server: {
+    host: "0.0.0.0", // Важно для доступа из контейнера
+    port: 5173,
+    watch: {
+      usePolling: true, // Для Docker на Windows/Mac
+    },
+    hmr: {
+      clientPort: 5173, // Для правильного WebSocket
+    },
+  },
+});

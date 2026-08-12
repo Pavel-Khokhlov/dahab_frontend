9f6729c01d3fe9b1cea06715d06a3ea9f97616b0
Pavel Khokhlov <gg.pavel.kh@gmail.com>
2026-08-12 05:32:35 +0300

    add avatar interface

--- src/assets/images/avatars/index.ts
+++ src/assets/images/avatars/index.ts
@@ -1,9 +1,9 @@
 import alexander_yakovlev from "./alexander_yakovlev.webp";
-import nathalie_ocean from "./nathalie.ocean.webp";
+import nathalie_ocean from "./nathalie_ocean.webp";
 import va_dim from "./va_dim.webp";
 import anna from "./anna.webp";
 
-const images = {
+const images: Record<string, string> = {
   alexander_yakovlev,
   nathalie_ocean,
   va_dim,
